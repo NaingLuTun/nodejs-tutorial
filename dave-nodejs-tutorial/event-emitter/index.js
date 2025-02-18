9d9e9e1c476b55840c99be831eb1b28670a11c79
Naing Lu Tun <nainglu7924@gmail.com>
2025-02-18 18:36:44 +0630

    event emitter

---
+++ dave-nodejs-tutorial/event-emitter/index.js
@@ -0,0 +1,18 @@
+const logEvents = require("./logEvent")
+
+const EventEmitter = require("events")
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on("log", (msg) => {
+    logEvents(msg)
+})
+
+setTimeout(() => {
+    myEmitter.emit("log", "log event emitted!")
+}, 2000)
